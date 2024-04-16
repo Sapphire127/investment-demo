69ecfc6700a9a4102d997d778a627ee0ed3199ca
wangxiangyue <>
2024-04-16 17:23:01 +0800

    install mui, adjust components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Home from './main/home/components/Home/Home.tsx';
+
+function App() {
+    return (
+        <div className="App">
+            <Home />
+        </div>
+    );
+}
+
+export default App;
