c2e2acf1bf92ab4d8088acaf6920c4d62c19f397
wangxiangyue <>
2024-04-17 12:07:02 +0800

    add antd Message

--- src/main/home/components/Home/Home.tsx
+++ src/main/home/components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MyForm from "./MyForm.tsx";
 import MySnackbar from "./MySnackbar.tsx";
+import MyMessage from "./MyMessage.tsx";
 
 export default function Home () {
     return (
         <>
             <MyForm />
             <MySnackbar />
+            <MyMessage />
         </>
     );
 }
