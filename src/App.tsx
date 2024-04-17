de477dd2a544291426f3399ec269a9dafcf5037a
wangxiangyue <>
2024-04-17 16:08:54 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Home from './main/home/components/Home/Home.tsx';
+import Fund from './main/fund/components/Fund/Fund.tsx';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 
 function App() {
     return (
-        <div className="App">
-            <Home />
-        </div>
+        <ConfigProvider>
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/*" element={
+                        <Routes>
+                            <Route path="myfund" element={<Fund />}></Route>
+                        </Routes>
+                    }
+                    ></Route>
+                </Routes>
+            </HashRouter>
+        </ConfigProvider>
     );
 }
 
---
+++ src/main/fund/components/Fund/Fund.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Fund() {
+    return (
+        <>12</>
+    )
+}
+
+export default Fund;
