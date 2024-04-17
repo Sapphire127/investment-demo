fd87ac205c3cd741dddf25c9ef1f410bb20f4826
wangxiangyue <>
2024-04-17 11:02:52 +0800

    add MySnackbar

--- src/main/home/components/Home/Home.tsx
+++ src/main/home/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MyForm from "./MyForm.tsx";
+import MySnackbar from "./MySnackbar.tsx";
 
 export default function Home () {
     return (
         <>
             <MyForm />
+            <MySnackbar />
         </>
     );
 }
